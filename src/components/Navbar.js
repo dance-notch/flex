ff4728f3f4f80ca77b95db1c7bc2def8ac792099
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-04-18 15:38:21 +0700

    Add: Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <nav className="px-[42px] w-full py-[22px]">
+      <ul className="flex justify-between items-center text-primary">
+        <div className="flex items-center gap-[54px]">
+          <li className="text-5xl font-bold uppercase">Flex</li>
+          <li className="text-2xl font-semibold text-2x">Find Curriculum</li>
+          <li className="text-2xl font-semibold">About us</li>
+        </div>
+        <li className="text-3xl font-semibold">Login</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
