4c3c96df067cba886c348b9b22a9f789c334316f
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-04-20 03:48:50 +0700

    Add: add draggable box

--- src/components/SubjectBox.js
+++ src/components/SubjectBox.js
@@ -1,15 +1,33 @@
 import React from "react";
+import { useDraggable } from "@dnd-kit/core";
+function SubjectBox({ id, codeId, course, color = "gray" }) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: id,
+    data: { codeId },
+  });
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
 
-function SubjectBox({ color = "gray" }) {
   return (
-    <div className={`w-[183px] h-[100px] border-[1px] rounded flex `}>
-      <div className={`h-full w-[25px] rounded-l bg-hack-gray-stroke`}></div>
-      <div className="w-full text-center flex flex-col justify-center items-center text-hack-gray-text">
-        <p className="font-semibold">3404117</p>
-        <p className="font-semibold">INTRO TO LAW</p>
-        <p>({3} credits)</p>
+    course && (
+      <div
+        ref={setNodeRef}
+        style={style}
+        {...listeners}
+        {...attributes}
+        className={`w-[183px] h-[100px] border-[1px] rounded flex `}
+      >
+        <div className={`h-full w-[25px] rounded-l bg-hack-gray-stroke`}></div>
+        <div className="w-full text-center flex flex-col justify-center items-center text-hack-gray-text">
+          <p className="font-semibold">{codeId}</p>
+          <p className="font-semibold">{course[codeId].shortName}</p>
+          <p>{course[codeId].credits} credits</p>
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
