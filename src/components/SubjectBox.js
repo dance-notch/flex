0b082b9e9c351487e396c69345a9a1dceb23e43b
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-04-19 20:18:24 +0700

    Add: Style Subject box

--- src/components/SubjectBox.js
+++ src/components/SubjectBox.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function SubjectBox() {
-  return <div></div>;
+function SubjectBox({ color = "gray" }) {
+  return (
+    <div className={`w-[183px] h-[100px] border-[1px] rounded flex `}>
+      <div className={`h-full w-[25px] rounded-l bg-hack-gray-stroke`}></div>
+      <div className="w-full text-center flex flex-col justify-center items-center text-hack-gray-text">
+        <p className="font-semibold">3404117</p>
+        <p className="font-semibold">INTRO TO LAW</p>
+        <p>({3} credits)</p>
+      </div>
+    </div>
+  );
 }
 
 export default SubjectBox;
