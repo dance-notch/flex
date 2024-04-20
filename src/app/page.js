3a2779c78d8ddd6623bb29077242b9373cd54f81
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-04-20 16:01:48 +0700

    Fix: fix drag and drop bugs

--- src/app/page.js
+++ src/app/page.js
@@ -4,44 +4,57 @@ import CurriculumSection from "@/components/CurriculumSection";
 import GenedSection from "@/components/GenedSection";
 import SummarySection from "@/components/SummarySection";
 import { DndContext } from "@dnd-kit/core";
-import { dataSemester, dataCourse } from "@/components/dataSemester";
+import { dataSemester, dataCourse } from "@/utilities/dataSemester";
 import { useState } from "react";
+import { useSensor, useSensors, MouseSensor } from "@dnd-kit/core";
 
 export default function Home() {
+  const sensors = useSensors(
+    useSensor(MouseSensor, {
+      // onActivation: (event) => console.log("onActivation", event), // Here!
+      activationConstraint: { distance: 5 },
+    })
+  );
   const [semester, setSemester] = useState(dataSemester);
   const [course, setCourse] = useState(dataCourse);
   return (
-    <main className="bg-white w-full h-full">
+    <main className="bg-white w-full h-full font-poppins">
       <Navbar />
       <DndContext
+        sensors={sensors}
         onDragEnd={(event) => {
           const { active, over } = event;
           const overId = over.id[2];
-          const activeYear = active.id[0] - 0;
-          const activeSemester = active.id[1] - 0;
-          const activeId = active.id[2];
+          const activeYear = active.id[0] ? active.id[0] - 0 : 0;
+          const activeSemester = active.id[1] ? active.id[1] - 0 : 0;
+          const activeId = active.id[2] ? active.id[2] : 0;
           const activeCodeId = active.data.current.codeId;
-          // console.log("active", active);
-          // console.log("over", over);
-          // console.log(
-          //   overId,
-          //   activeYear,
-          //   activeSemester,
-          //   activeId,
-          //   activeCodeId
-          // );
+          console.log("active", active);
+          console.log("over", over);
+          console.log(
+            overId,
+            activeYear,
+            activeSemester,
+            activeId,
+            activeCodeId
+          );
           setSemester((prevData) =>
             prevData.map((item, index) => {
               if (
                 over.data.current.year === item.year &&
                 over.data.current.semester === item.semester
               ) {
-                if (activeId === overId) {
+                console.log(activeId, overId);
+                if (
+                  over.data.current.year === activeYear &&
+                  over.data.current.semester === activeSemester
+                ) {
                   return {
                     ...item,
                     dropbox: {
                       ...item.dropbox,
                       [overId]: activeCodeId,
+                      [activeId]: null,
                     },
                   };
                 }
@@ -50,7 +63,6 @@ export default function Home() {
                   dropbox: {
                     ...item.dropbox,
                     [overId]: activeCodeId,
-                    [activeId]: null,
                   },
                 };
               } else if (
@@ -66,17 +78,35 @@ export default function Home() {
               }
             })
           );
-          setCourse((prevData) => ({
-            ...prevData,
-            [activeCodeId]: {
-              ...prevData[activeCodeId],
-              dropbox:
-                "" +
-                over.data.current.year +
-                over.data.current.semester +
-                overId,
-            },
-          }));
+
+          setCourse((prevData) =>
+            prevData.map((item, index) => {
+              if (item.courseNo === activeCodeId) {
+                return {
+                  ...item,
+                  dropbox:
+                    "" +
+                    over.data.current.year +
+                    over.data.current.semester +
+                    overId,
+                };
+              } else {
+                return item;
+              }
+            })
+          );
+
+          // setCourse((prevData) => ({
+          //   ...prevData,
+          //   [activeCodeId]: {
+          //     ...prevData[activeCodeId],
+          //     dropbox:
+          //       "" +
+          //       over.data.current.year +
+          //       over.data.current.semester +
+          //       overId,
+          //   },
+          // }));
         }}
       >
         <CurriculumSection
@@ -85,8 +115,8 @@ export default function Home() {
           course={course}
           setCourse={setCourse}
         />
-        <GenedSection />
       </DndContext>
+      <GenedSection course={course} />
       <SummarySection />
     </main>
   );
