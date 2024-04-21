87a12717812070aff3aa82ef48d394419ae72fab
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-04-21 11:41:20 +0700

    font

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="font-poppins">
       <body>{children}</body>
     </html>
   );
